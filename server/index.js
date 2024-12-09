b3a6542dcb1c18a56b0aff5fcc010e721dbc468f
A.Sandeep avala <sandeepavala2@gmail.com>
2024-12-09 11:45:06 +0530

    fifth commit

--- server/index.js
+++ server/index.js
@@ -8,9 +8,23 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-
-
 mongoose.connect("mongodb://127.0.0.1:27017/worker")
+
+app.post("/login", (req,res)=>{
+    const {number, password} = req.body;
+    WorkerModel.findOne({number: number})
+    .then(user =>{
+        if(user){
+        if(user.password === password){
+            res.json("Success")
+        }else{
+            res.json("the password is incorrect")
+        }
+    }else{
+        res.json("No record existed")
+    }
+    })
+})
 
 app.post('/register', (req,res)=>{
     WorkerModel.create(req.body)
